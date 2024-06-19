0303fe6684c7afe4a2e04c30c56588fb447b26e7
Yasaru2004 <warleader330@gmail.com>
2024-06-19 13:08:01 +0530

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// app.js
-
 function calc() {
     let number01 = document.getElementById("number01").value;
     let number02 = document.getElementById("number02").value;
@@ -7,15 +5,15 @@ function calc() {
     let op = document.getElementById("op").value;
     let lblOutput = document.getElementById("output");
 
-    switch (op) {
-        case "+": lblOutput.innerHTML = parseFloat(number01) + parseFloat(number02); break;
-        case "-": lblOutput.innerHTML = parseFloat(number01) - parseFloat(number02); break;
-        case "*": lblOutput.innerHTML = parseFloat(number01) * parseFloat(number02); break;
-        case "/":
-            if (parseFloat(number02) != 0)
-                lblOutput.innerHTML = parseFloat(number01) / parseFloat(number02);
+    switch(op){
+        case "+": lblOutput.textContent = parseFloat(number01) + parseFloat(number02); break;
+        case "-": lblOutput.textContent = parseFloat(number01) - parseFloat(number02); break;
+        case "*": lblOutput.textContent = parseFloat(number01) * parseFloat(number02); break;
+        case "/": 
+            if(parseFloat(number02) !== 0)
+                lblOutput.textContent = parseFloat(number01) / parseFloat(number02); 
             else
-                lblOutput.innerHTML = "Error: Division by zero";
+                lblOutput.textContent = "Error: Division by zero"; 
             break;
     }
 }
