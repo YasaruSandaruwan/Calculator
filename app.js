6348535fd6e5d7f3839d95f930df7d4ed8e260c1
Yasaru2004 <warleader330@gmail.com>
2024-06-19 12:53:40 +0530

    seconed commit

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+// app.js
+
 function calc() {
-    let number01 = document.getElementById("number 01").value;
-    let number02 = document.getElementById("number 02").value;
+    let number01 = document.getElementById("number01").value;
+    let number02 = document.getElementById("number02").value;
 
     let op = document.getElementById("op").value;
     let lblOutput = document.getElementById("output");
 
-    switch(op){
+    switch (op) {
         case "+": lblOutput.innerHTML = parseFloat(number01) + parseFloat(number02); break;
         case "-": lblOutput.innerHTML = parseFloat(number01) - parseFloat(number02); break;
         case "*": lblOutput.innerHTML = parseFloat(number01) * parseFloat(number02); break;
-        case "/": 
-            if(parseFloat(number02) != 0)
-                lblOutput.innerHTML = parseFloat(number01) / parseFloat(number02); 
+        case "/":
+            if (parseFloat(number02) != 0)
+                lblOutput.innerHTML = parseFloat(number01) / parseFloat(number02);
             else
-                lblOutput.innerHTML = "Error: Division by zero"; 
+                lblOutput.innerHTML = "Error: Division by zero";
             break;
     }
 }
